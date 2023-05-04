c5845c9334fc350bcc45f0ddc4c5ed5bc7c5e948
COT-5405 <119132147+COT-5405@users.noreply.github.com>
2023-05-04 18:29:34 -0400

    Create data_preprocessing.py

---
+++ data_preprocessing.py
@@ -0,0 +1,19 @@
+import torchvision.transforms as transforms
+
+# Define data augmentation transforms
+train_transforms = transforms.Compose([
+    transforms.RandomResizedCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
+
+test_transforms = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
